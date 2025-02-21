ee93028d7b36f3f88e6926fdb5ae6aa17c5144de
Ruchi Yadav <shalini.mishra@iphtechnologies.com>
2025-02-21 15:36:48 +0530

    commit

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //load the calculator fragment
+//        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, CalculatorFragment()).commit()
+        loadFragment(CalculatorFragment())
     }
+    private fun loadFragment(fragment: Fragment) {
+        // Create a FragmentManager
+        val fm = supportFragmentManager
+        // Begin the transaction and replace the Fragment
+        val fragmentTransaction = fm.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit() // Save the changes
+    }
+
 }
